0ec50fd0a0649048685f5b8efabb7fe7fd790eee
Drew Mace <dremac1@live.com>
2017-04-19 20:48:42 +0000

    removed authorization from app_controller

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -2,7 +2,6 @@ class ApplicationPolicy
   attr_reader :user, :wiki
 
   def initialize(user, wiki)
-    raise Pundit::NotAuthorizedError, "must be logged in" unless user
     @user = user
     @wiki = wiki
   end
