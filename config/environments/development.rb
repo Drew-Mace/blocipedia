f341fd883a14bf251444d963288db69a337b7be9
Drew Mace <dremac1@live.com>
2017-04-23 22:17:38 +0000

    changed dev settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,16 +17,16 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   
   # default URL options for the Devise mailer
-  config.action_mailer.default_url_options = { :host => 'https://ide.c9.io/babydontsaythat/blocipedia' }
+  config.action_mailer.default_url_options = { :host => '0.0.0.0.8080' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    :address              => 'smtp.live.com',
-    :port                 => 25,
+    :address              => 'smtp.c9.io',
+    :port                 => 2525,
     :domain               => 'mail.live.com',
-    :user_name            => 'dremac1@live.com',
-    :password             => 'o25ec8000',
-    :authentication => :plain,
+    :user_name            => ENV['SENDGRID_USERNAME'],
+    :password             => ENV['SENDGRID_PASSWORD'],
+    :authentication => 'login',
     :enable_starttls_auto => true,
     :ssl =>true
 }
